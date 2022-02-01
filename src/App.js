6f2c2b3d8aaed5f030fe239b3e65d4d40142ac69
jose zambrano <joseyzambranovpe@gmail.com>
2022-01-31 23:20:26 -0500

    created count and add and subtract button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
 import './App.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+  const [time,setTime]=useState(1500)
+  const [play,setPlay]=useState(false)
+
+  const compInterval = setTimeout(()=>{
+    if(play&&time){
+      setTime(time-1)
+  }
+    },1000)
+
+  const startTimeFormat=()=>{
+    setPlay(true)
+  }
+
+  const stopTimeFormat=()=>{
+    clearTimeout(compInterval)
+    setPlay(false)
+  }
+
+  const sumTime=()=>{
+    setTime(time+60)
+  }
+
+  const subTime=()=>{
+    setTime(time-60)
+  }
+
+  const timeFormat =()=>{
+    const min= Math.floor(time/60)
+    const seg= time - min * 60
+    const formatSeg=seg<10?"0"+ seg : seg;
+    const formatMin=min<10?"0"+ min : min;
+    return `${formatMin} : ${formatSeg}`
+  }
+
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div  className='container text-center'>
+      <div>
+        <h2 >Session Label</h2>
+      <h3 id="time-left" >{timeFormat()}</h3>
+      <button onClick={stopTimeFormat}>stop</button>
+      <button onClick={startTimeFormat}>start</button>  
+      </div> 
+      <div id="break-label">Break Length</div>
+      <h3>5</h3>
+      <button onClick={sumTime}>+</button>
+      <button onClick={subTime}>-</button>
     </div>
   );
 }
